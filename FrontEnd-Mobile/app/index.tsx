7ab47631d9e45b3600664a939e871f69b8d31327
Manuel Garcia-Nieto <alenmaapps@gmail.com>
2024-10-18 23:35:06 +0200

    Add entire app

---
+++ FrontEnd-Mobile/app/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from 'react-native'
+
+export default function Index() {
+	return (
+		<View className='bg-gray-800 flex-1 items-center justify-center'>
+			<Text className='text-2xl text-yellow-500 '>TastyMeal DAM Project</Text>
+		</View>
+	)
+}
