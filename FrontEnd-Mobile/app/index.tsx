8e67a4fda3479574208ea3b6387e905efc669e5d
Manuel Garcia-Nieto <alenmaapps@gmail.com>
2024-10-20 12:36:07 +0200

    add welcome screen

--- FrontEnd-Mobile/app/index.tsx
+++ FrontEnd-Mobile/app/index.tsx
@@ -1,9 +1,67 @@
-import { Text, View } from 'react-native'
+// app/welcome.jsx
+import React from 'react'
+import { View, Text, Image, Button, StyleSheet, TouchableOpacity } from 'react-native'
+import { useRouter } from 'expo-router'
 
-export default function Index() {
+export default function WelcomeScreen() {
+	const router = useRouter()
+
+	const handleRegister = () => {
+		//router.push('/register');
+	}
+
+	const handleLogin = () => {
+		//router.push('/login');
+	}
+
 	return (
-		<View className='bg-gray-800 flex-1 items-center justify-center'>
-			<Text className='text-2xl text-yellow-500 '>TastyMeal DAM Project</Text>
+		<View className='flex-1 p-5 items-center justify-between bg-lime-500'>
+			<Image className='w-full h-1/2 mt-5 rounded-3xl  ' source={require('../assets/images/main-picture.png')} resizeMode='cover' />
+
+			<Text className='text-3xl text-center font-black text-slate-700'>Welcome to Tasty Meal!</Text>
+
+			<Text className='text-lg mb-4 text-center text-slate-700'> Discover a world of culinary delights with us!</Text>
+
+			<TouchableOpacity style={styles.button} onPress={handleRegister}>
+				<Text style={styles.buttonText}>Getting Started</Text>
+			</TouchableOpacity>
+
+			<TouchableOpacity onPress={handleLogin}>
+				<Text className='text-slate-800 text-center mb-12 text-lg'>Already have an account? Login</Text>
+			</TouchableOpacity>
 		</View>
 	)
 }
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		padding: 16,
+		alignItems: 'center',
+		justifyContent: 'space-between'
+	},
+	image: {
+		width: '100%',
+		height: '50%',
+		marginTop: 50
+	},
+	button: {
+		width: '80%',
+		backgroundColor: '#000000',
+		paddingVertical: 15,
+		borderRadius: 8,
+		marginBottom: 20,
+		alignItems: 'center'
+	},
+	buttonText: {
+		color: '#FFFFFF',
+		fontSize: 20,
+		fontWeight: '600'
+	},
+	loginText: {
+		color: '#007AFF',
+		textAlign: 'center',
+		marginBottom: 40,
+		textDecorationLine: 'underline'
+	}
+})
