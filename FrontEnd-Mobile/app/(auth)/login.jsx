7f0ebf1c812f907430b987d8de58938c51222c17
Manuel Garcia-Nieto <alenmaapps@gmail.com>
2024-10-21 16:37:45 +0200

    add validation and message to backend

--- FrontEnd-Mobile/app/(auth)/login.jsx
+++ FrontEnd-Mobile/app/(auth)/login.jsx
@@ -1,117 +1,107 @@
-import React, { useState } from 'react';
-import { View, Text, TextInput, TouchableOpacity, StyleSheet } from 'react-native';
-import { Link, router } from 'expo-router';
+import React, { useState } from 'react'
+import { View, Text, TextInput, TouchableOpacity, StyleSheet, Alert } from 'react-native'
+import { Link, router } from 'expo-router'
 
 export default function LoginForm() {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+	const [username, setUsername] = useState('')
+	const [password, setPassword] = useState('')
 
-  const handleLogin = () => {
-    console.log('Email:', email);
-    console.log('Password:', password);
-    router.push('/(main)/home');
-  };
+	const handleLogin = () => {
+		// if (!username || !password) {
+		// 	Alert.alert('Error', 'Please fill in all the fields.')
+		// 	return
+		// }
+		console.log('Username:', username)
+		console.log('Password:', password)
+		router.push('/(main)/home')
+	}
 
-  return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Hey,</Text>
-      <Text style={styles.title}>Welcome Back</Text>
-      <Text style={styles.subtitle}>Please login to continue</Text>
+	return (
+		<View style={styles.container}>
+			<Text style={styles.title}>Hey,</Text>
+			<Text style={styles.title}>Welcome Back</Text>
+			<Text style={styles.subtitle}>Please login to continue</Text>
 
-      <TextInput
-        style={styles.input}
-        placeholder="Enter your username"
-        value={email}
-        onChangeText={setEmail}
-        keyboardType="email-address"
-      />
-      <TextInput
-        style={styles.input}
-        placeholder="Enter your password"
-        value={password}
-        onChangeText={setPassword}
-        secureTextEntry
-      />
+			<TextInput style={styles.input} placeholder='Enter your username' value={username} onChangeText={setUsername} keyboardType='email-address' />
+			<TextInput style={styles.input} placeholder='Enter your password' value={password} onChangeText={setPassword} secureTextEntry />
 
-      <View style={styles.forgotPasswordContainer}>
-        <TouchableOpacity>
-          <Text style={styles.forgotPasswordText}>Forgot Password?</Text>
-        </TouchableOpacity>
-      </View>
+			<View style={styles.forgotPasswordContainer}>
+				<TouchableOpacity>
+					<Text style={styles.forgotPasswordText}>Forgot Password?</Text>
+				</TouchableOpacity>
+			</View>
 
-      <TouchableOpacity style={styles.button} onPress={handleLogin}>
-        <Text style={styles.buttonText}>Login</Text>
-      </TouchableOpacity>
+			<TouchableOpacity style={styles.button} onPress={handleLogin}>
+				<Text style={styles.buttonText}>Login</Text>
+			</TouchableOpacity>
 
-      <Text style={styles.signupText}>
-        Don't have an account?{' '}
-        <Link href="signUp" style={styles.signupLink}>
-          Sign Up
-        </Link>
-      </Text>
-    </View>
-  );
+			<Text style={styles.signupText}>
+				Don't have an account?{' '}
+				<Link href='signUp' style={styles.signupLink}>
+					Sign Up
+				</Link>
+			</Text>
+		</View>
+	)
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    paddingHorizontal: 20,
-    backgroundColor: '#fff',
-  },
-  title: {
-    fontSize: 28,
-    fontWeight: 'bold',
-    textAlign: 'left',
-  },
-  subtitle: {
-    fontSize: 14,
-    textAlign: 'left',
-    color: '#7e7e7e',
-    marginBottom: 20,
-  },
-  input: {
-    borderWidth: 1,
-    borderColor: '#ccc',
-    borderRadius: 25,
-    paddingVertical: 10,
-    paddingHorizontal: 15,
-    marginBottom: 15,
-  },
-  forgotPasswordContainer: {
-    alignItems: 'flex-end',
-    marginBottom: 20,
-  },
-  forgotPasswordText: {
-    color: '#7e7e7e',
-    fontWeight: 'bold',
-  },
-  button: {
-    backgroundColor: '#28a745',
-    borderRadius: 25,
-    paddingVertical: 15,
-    alignItems: 'center',
-    marginBottom: 20,
-    // Sombra para iOS
-    shadowColor: '#000', // Color de la sombra
-    shadowOffset: { width: 0, height: 4 }, // Desplazamiento de la sombra
-    shadowOpacity: 0.3, // Opacidad de la sombra
-    shadowRadius: 4.65, // Radio de la sombra
-    // Sombra para Android
-    elevation: 8, // Nivel de elevación para Android
-  },
-  buttonText: {
-    color: '#fff',
-    fontSize: 18,
-    fontWeight: 'bold',
-  },
-  signupText: {
-    textAlign: 'center',
-    color: '#7e7e7e',
-  },
-  signupLink: {
-    color: '#28a745',
-    fontWeight: 'bold',
-  },
-});
+	container: {
+		flex: 1,
+		justifyContent: 'center',
+		paddingHorizontal: 20,
+		backgroundColor: '#fff'
+	},
+	title: {
+		fontSize: 28,
+		fontWeight: 'bold',
+		textAlign: 'left'
+	},
+	subtitle: {
+		fontSize: 14,
+		textAlign: 'left',
+		color: '#7e7e7e',
+		marginBottom: 20
+	},
+	input: {
+		borderWidth: 1,
+		borderColor: '#ccc',
+		borderRadius: 25,
+		paddingVertical: 10,
+		paddingHorizontal: 15,
+		marginBottom: 15
+	},
+	forgotPasswordContainer: {
+		alignItems: 'flex-end',
+		marginBottom: 20
+	},
+	forgotPasswordText: {
+		color: '#7e7e7e',
+		fontWeight: 'bold'
+	},
+	button: {
+		backgroundColor: '#28a745',
+		borderRadius: 25,
+		paddingVertical: 15,
+		alignItems: 'center',
+		marginBottom: 20,
+		shadowColor: '#000',
+		shadowOffset: { width: 0, height: 4 },
+		shadowOpacity: 0.3,
+		shadowRadius: 4.65,
+		elevation: 8
+	},
+	buttonText: {
+		color: '#fff',
+		fontSize: 18,
+		fontWeight: 'bold'
+	},
+	signupText: {
+		textAlign: 'center',
+		color: '#7e7e7e'
+	},
+	signupLink: {
+		color: '#28a745',
+		fontWeight: 'bold'
+	}
+})
--- FrontEnd-Mobile/app/(auth)/signUp.jsx
+++ FrontEnd-Mobile/app/(auth)/signUp.jsx
@@ -1,99 +1,152 @@
 import { router } from 'expo-router'
 import React, { useState } from 'react'
-import { View, Text, TextInput, TouchableOpacity, StyleSheet } from 'react-native'
+import { View, Text, TextInput, TouchableOpacity, StyleSheet, Alert } from 'react-native'
 
 export default function SignUpForm() {
-	const [name, setName] = useState('')
-	const [email, setEmail] = useState('')
-	const [password, setPassword] = useState('')
+  const [username, setUsername] = useState('')
+  const [email, setEmail] = useState('')
+  const [firstName, setFirstName] = useState('')
+  const [lastName, setLastName] = useState('')
+  const [password, setPassword] = useState('')
 
-	const handleSignUp = () => {
-		console.log('Name:', name)
-		console.log('Email:', email)
-		console.log('Password:', password)
-		router.push('/(main)/home')
-	}
+  const validateEmail = (email) => {
+    const re = /\S+@\S+\.\S+/
+    return re.test(email)
+  }
 
-	return (
-		<View style={styles.container}>
-			<Text style={styles.title}>Let's Get Started</Text>
-			<Text style={styles.subtitle}>Please fill the details to create an account</Text>
+  const handleSignUp = () => {
+    // if (!username || !email || !firstName || !lastName || !password) {
+    //   Alert.alert('Error', 'Please fill in all the fields.')
+    //   return
+    // }
 
-			<TextInput style={styles.input} placeholder='Enter your name' value={name} onChangeText={setName} />
-			<TextInput style={styles.input} placeholder='Enter your email' value={email} onChangeText={setEmail} keyboardType='email-address' />
-			<TextInput style={styles.input} placeholder='Enter your password' value={password} onChangeText={setPassword} secureTextEntry />
+    // if (!validateEmail(email)) {
+    //   Alert.alert('Error', 'Please enter a valid email address.')
+    //   return
+    // }
 
-			<TouchableOpacity style={styles.button} onPress={handleSignUp}>
-				<Text style={styles.buttonText}>Sign Up</Text>
-			</TouchableOpacity>
+    // if (password.length < 6) {
+    //   Alert.alert('Error', 'Password must be at least 6 characters long.')
+    //   return
+    // }
 
-			<Text style={styles.loginText}>
-				Already have an account?{' '}
-				<Text
-					style={styles.loginLink}
-					onPress={() => {
-						console.log('Navigate to Login')
-						router.push('/(auth)/login')
-					}}>
-					Login
-				</Text>
-			</Text>
-		</View>
-	)
+    // send data to bakcend
+    console.log('Username:', username)
+    console.log('Email:', email)
+    console.log('First Name:', firstName)
+    console.log('Last Name:', lastName)
+    console.log('Password:', password)
+    router.push('/(main)/home')
+  }
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Let's Get Started</Text>
+      <Text style={styles.subtitle}>Please fill in the details to create an account</Text>
+
+      <TextInput
+        style={styles.input}
+        placeholder='Enter your username'
+        value={username}
+        onChangeText={setUsername}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder='Enter your email'
+        value={email}
+        onChangeText={setEmail}
+        keyboardType='email-address'
+      />
+      <TextInput
+        style={styles.input}
+        placeholder='Enter your first name'
+        value={firstName}
+        onChangeText={setFirstName}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder='Enter your last name'
+        value={lastName}
+        onChangeText={setLastName}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder='Enter your password'
+        value={password}
+        onChangeText={setPassword}
+        secureTextEntry
+      />
+
+      <TouchableOpacity style={styles.button} onPress={handleSignUp}>
+        <Text style={styles.buttonText}>Sign Up</Text>
+      </TouchableOpacity>
+
+      <Text style={styles.loginText}>
+        Already have an account?{' '}
+        <Text
+          style={styles.loginLink}
+          onPress={() => {
+            router.push('/(auth)/login')
+          }}>
+          Login
+        </Text>
+      </Text>
+    </View>
+  )
 }
 
 const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		justifyContent: 'center',
-		paddingHorizontal: 20,
-		backgroundColor: '#fff'
-	},
-	title: {
-		fontSize: 28,
-		fontWeight: 'bold',
-		textAlign: 'center',
-		marginBottom: 10
-	},
-	subtitle: {
-		fontSize: 14,
-		textAlign: 'center',
-		color: '#7e7e7e',
-		marginBottom: 20
-	},
-	input: {
-		borderWidth: 1,
-		borderColor: '#ccc',
-		borderRadius: 25,
-		paddingVertical: 10,
-		paddingHorizontal: 15,
-		marginBottom: 15
-	},
-	button: {
-		backgroundColor: '#28a745',
-		borderRadius: 25,
-		paddingVertical: 15,
-		alignItems: 'center',
-		marginBottom: 20,
-		//iOS
-		shadowColor: '#000', // Color de la sombra
-		shadowOffset: { width: 0, height: 4 },
-		shadowOpacity: 0.3, // Opacidad de la sombra
-		shadowRadius: 4.65, // Radio de la sombra
-		//Android
-		elevation: 8
-	},
-	buttonText: {
-		color: '#fff',
-		fontSize: 18,
-		fontWeight: 'bold'
-	},
-	loginText: {
-		textAlign: 'center',
-		color: '#7e7e7e'
-	},
-	loginLink: {
-		color: '#28a745',
-		fontWeight: 'bold'
-	}
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    paddingHorizontal: 20,
+    backgroundColor: '#fff'
+  },
+  title: {
+    fontSize: 28,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    marginBottom: 10
+  },
+  subtitle: {
+    fontSize: 14,
+    textAlign: 'center',
+    color: '#7e7e7e',
+    marginBottom: 20
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: '#ccc',
+    borderRadius: 25,
+    paddingVertical: 10,
+    paddingHorizontal: 15,
+    marginBottom: 15
+  },
+  button: {
+    backgroundColor: '#28a745',
+    borderRadius: 25,
+    paddingVertical: 15,
+    alignItems: 'center',
+    marginBottom: 20,
+    // Sombra para iOS
+    shadowColor: '#000', // Color de la sombra
+    shadowOffset: { width: 0, height: 4 },
+    shadowOpacity: 0.3, // Opacidad de la sombra
+    shadowRadius: 4.65, // Radio de la sombra
+    // Elevación para Android
+    elevation: 8
+  },
+  buttonText: {
+    color: '#fff',
+    fontSize: 18,
+    fontWeight: 'bold'
+  },
+  loginText: {
+    textAlign: 'center',
+    color: '#7e7e7e'
+  },
+  loginLink: {
+    color: '#28a745',
+    fontWeight: 'bold'
+  }
 })
