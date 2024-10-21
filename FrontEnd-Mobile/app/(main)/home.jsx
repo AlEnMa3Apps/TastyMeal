d486ad7da293647d28aa498f9fdf34253c91aef2
Manuel Garcia-Nieto <alenmaapps@gmail.com>
2024-10-21 17:10:19 +0200

    add logout button

--- FrontEnd-Mobile/app/(main)/home.jsx
+++ FrontEnd-Mobile/app/(main)/home.jsx
@@ -1,72 +1,52 @@
+// app/main/home.jsx
 import React from 'react'
-import { View, Text, FlatList, Image, StyleSheet } from 'react-native'
+import { View, Text, FlatList, Image, TouchableOpacity } from 'react-native'
+import { useRouter } from 'expo-router'
+// import { useAuth } from '../../context/AuthContext'
 
 // const recipes = [
-//   {
-//     id: '1',
-//     title: 'Ensalada César',
-//     image: require('../../assets/cesar-salad.jpg'),
-//   },
-//   {
-//     id: '2',
-//     title: 'Paella Valenciana',
-//     image: require('../../assets/paella.jpg'),
-//   },
-//   {
-//     id: '3',
-//     title: 'Tacos al Pastor',
-//     image: require('../../assets/tacos.jpg'),
-//   },
-// ];
+// 	{
+// 		id: '1',
+// 		title: 'Ensalada César',
+// 		image: require('../../assets/cesar-salad.jpg')
+// 	},
+// 	{
+// 		id: '2',
+// 		title: 'Paella Valenciana',
+// 		image: require('../../assets/paella.jpg')
+// 	},
+// 	{
+// 		id: '3',
+// 		title: 'Tacos al Pastor',
+// 		image: require('../../assets/tacos.jpg')
+// 	}
+// ]
 
-export default function Home() {
+export default function HomeScreen() {
+	const router = useRouter()
+	// const { logout } = useAuth()
+
+	const handleLogout = () => {
+		// logout() // Limpia el estado de autenticación
+		router.replace('/') // Redirige al usuario a la pantalla de login
+	}
+
 	const renderItem = ({ item }) => (
-		<View style={styles.card}>
-			<Image source={item.image} style={styles.image} resizeMode='cover' />
-			<Text style={styles.title}>{item.title}</Text>
+		<View className='mb-5 rounded-lg overflow-hidden bg-gray-200'>
+			<Image source={item.image} className='w-full h-48' resizeMode='cover' />
+			<Text className='text-xl p-4'>{item.title}</Text>
 		</View>
 	)
 
 	return (
 		<View className='flex-1 bg-green-500 items-center justify-center'>
-			<Text className='text-4xl mt-52 text-white font-bold'> Tasty Meal Recipes </Text>
-			<FlatList
-				// data={recipes}
-				renderItem={renderItem}
-				keyExtractor={(item) => item.id}
-				contentContainerStyle={styles.list}
-			/>
+			<View className='pt-10 px-4 flex-row justify-between items-center'>
+				<Text className='text-4xl text-white font-bold'>Tasty Meal Recipes</Text>
+				<TouchableOpacity onPress={handleLogout} className='py-2 px-3 mt-52 bg-red-600 rounded-lg'>
+					<Text className='text-white font-semibold'>Log Out</Text>
+				</TouchableOpacity>
+			</View>
+			{/* <FlatList data={recipes} renderItem={renderItem} keyExtractor={(item) => item.id} contentContainerStyle={{ paddingHorizontal: 16, paddingTop: 20 }} /> */}
 		</View>
 	)
 }
-
-const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		paddingTop: 40,
-		backgroundColor: '#F'
-	},
-	header: {
-		fontSize: 28,
-		fontWeight: 'bold',
-		paddingHorizontal: 16,
-		marginBottom: 20
-	},
-	list: {
-		paddingHorizontal: 16
-	},
-	card: {
-		marginBottom: 20,
-		borderRadius: 8,
-		overflow: 'hidden',
-		backgroundColor: '#F8F8F8'
-	},
-	image: {
-		width: '100%',
-		height: 200
-	},
-	title: {
-		fontSize: 20,
-		padding: 16
-	}
-})
