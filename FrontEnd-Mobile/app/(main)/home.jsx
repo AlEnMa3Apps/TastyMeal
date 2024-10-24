2ee6b8b9f24768f8749e1190ea9672194c14998d
Manuel Garcia-Nieto <alenmaapps@gmail.com>
2024-10-24 14:59:39 +0200

    add some recipes

--- FrontEnd-Mobile/app/(main)/home.jsx
+++ FrontEnd-Mobile/app/(main)/home.jsx
@@ -1,25 +1,25 @@
 import React from 'react'
-import { View, Text, FlatList, Image, TouchableOpacity } from 'react-native'
+import { View, Text, FlatList, Image, TouchableOpacity, ScrollView } from 'react-native'
 import { useRouter } from 'expo-router'
 import { useAuth } from '../../context/AuthContext'
 
-// const recipes = [
-// 	{
-// 		id: '1',
-// 		title: 'Ensalada César',
-// 		image: require('../../assets/cesar-salad.jpg')
-// 	},
-// 	{
-// 		id: '2',
-// 		title: 'Paella Valenciana',
-// 		image: require('../../assets/paella.jpg')
-// 	},
-// 	{
-// 		id: '3',
-// 		title: 'Tacos al Pastor',
-// 		image: require('../../assets/tacos.jpg')
-// 	}
-// ]
+const recipes = [
+	{
+		id: '1',
+		title: 'Ensalada César',
+		image: require('../../assets/images/cesar-salad.png')
+	},
+	{
+		id: '2',
+		title: 'Paella Valenciana',
+		image: require('../../assets/images/paella.png')
+	},
+	{
+		id: '3',
+		title: 'Tacos al Pastor',
+		image: require('../../assets/images/tacos.png')
+	}
+]
 
 export default function HomeScreen() {
 	const router = useRouter()
@@ -38,17 +38,17 @@ export default function HomeScreen() {
 	)
 
 	return (
-		<View className='flex-1 bg-green-500 items-center justify-center'>
-			<View className='pt-10 px-4 flex-col justify-center items-center'>
+		<View className='flex-1 bg-green-500 items-center justify-center p-5'>
+			<View className='pt-5 px-4 flex-col justify-center items-center mt-64'>
 				<Text className='text-4xl text-white font-bold mb-5'>Tasty Meal Recipes</Text>
 				<Text className='text-4xl text-black font-bold'>Normal User</Text>
-				<Text className='text-base text-white' >Token: {token}</Text>
-				<Text className='text-lg text-white' >Role: {role}</Text>
-				<TouchableOpacity onPress={handleLogout} className='py-2 px-3 mt-52 bg-red-600 rounded-lg'>
+				<TouchableOpacity onPress={handleLogout} className='py-2 px-3 mt-5 bg-red-600 rounded-lg'>
 					<Text className='text-white font-semibold text-lg shadow-lg shadow-black'>Log Out</Text>
 				</TouchableOpacity>
 			</View>
-			{/* <FlatList data={recipes} renderItem={renderItem} keyExtractor={(item) => item.id} contentContainerStyle={{ paddingHorizontal: 16, paddingTop: 20 }} /> */}
+			<View className='pt-5 px-8 mt-5' >
+				<FlatList data={recipes} renderItem={renderItem} keyExtractor={(item) => item.id} contentContainerStyle={{ paddingHorizontal: 16, paddingTop: 20 }} />
+			</View>
 		</View>
 	)
 }
--- FrontEnd-Mobile/app/(main)/homeVIP.jsx
+++ FrontEnd-Mobile/app/(main)/homeVIP.jsx
@@ -3,23 +3,23 @@ import { View, Text, FlatList, Image, TouchableOpacity } from 'react-native'
 import { useRouter } from 'expo-router'
 import { useAuth } from '../../context/AuthContext'
 
-// const recipes = [
-// 	{
-// 		id: '1',
-// 		title: 'Ensalada César',
-// 		image: require('../../assets/cesar-salad.jpg')
-// 	},
-// 	{
-// 		id: '2',
-// 		title: 'Paella Valenciana',
-// 		image: require('../../assets/paella.jpg')
-// 	},
-// 	{
-// 		id: '3',
-// 		title: 'Tacos al Pastor',
-// 		image: require('../../assets/tacos.jpg')
-// 	}
-// ]
+const recipes = [
+	{
+		id: '1',
+		title: 'Ensalada César',
+		image: require('../../assets/images/cesar-salad.png')
+	},
+	{
+		id: '2',
+		title: 'Paella Valenciana',
+		image: require('../../assets/images/paella.png')
+	},
+	{
+		id: '3',
+		title: 'Tacos al Pastor',
+		image: require('../../assets/images/tacos.png')
+	}
+]
 
 export default function HomeScreenVIP() {
 	const router = useRouter()
@@ -38,15 +38,15 @@ export default function HomeScreenVIP() {
 	)
 
 	return (
-		<View className='flex-1 bg-green-500 items-center justify-center'>
-			<View className='pt-10 px-4 flex-col justify-center items-center'>
+		<View className='flex-1 bg-green-500 items-center justify-center p-5'>
+			<View className='pt-10 px-4 flex-col justify-center items-center mt-20'>
 				<Text className='text-4xl text-white font-bold mb-5'>Tasty Meal Recipes</Text>
 				<Text className='text-4xl text-black font-bold'>VIP User</Text>
-				<TouchableOpacity onPress={handleLogout} className='py-2 px-3 mt-52 bg-red-600 rounded-lg'>
+				<TouchableOpacity onPress={handleLogout} className='py-2 px-3 mt-5 bg-red-600 rounded-lg'>
 					<Text className='text-white font-semibold text-lg shadow-lg shadow-black'>Log Out</Text>
 				</TouchableOpacity>
 			</View>
-			{/* <FlatList data={recipes} renderItem={renderItem} keyExtractor={(item) => item.id} contentContainerStyle={{ paddingHorizontal: 16, paddingTop: 20 }} /> */}
+			<FlatList data={recipes} renderItem={renderItem} keyExtractor={(item) => item.id} contentContainerStyle={{ paddingHorizontal: 16, paddingTop: 20 }} />
 		</View>
 	)
 }
